8d2a6a1fe558e592c9c4c4e8f3e3966864fb52ea
christonomous <chris@brane.media>
2025-11-12 14:07:58 +0100

    add locales

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+// Initialize i18n
+const { locale } = useI18n()
+
+// Set locale from cookie if available
+const cookieLang = useCookie('i18n_redirected')
+if (cookieLang.value) {
+  locale.value = cookieLang.value
+}
+</script>
