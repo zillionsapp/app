c2e47d83249f8318fd32a7a094f45a6b743fa771
christonomous <chris@brane.media>
2026-01-06 18:29:56 +0100

    vercel 404 prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,16 +50,6 @@ export default defineNuxtConfig({
     }
   },
   devtools: { enabled: true },
-  routeRules: {
-    '/api/**': {
-      cors: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-      }
-    }
-  },
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
