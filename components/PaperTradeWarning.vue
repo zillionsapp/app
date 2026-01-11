c8b495901f5280410bd0265f796c6a3c602553cc
christonomous <chris@brane.media>
2026-01-11 20:27:47 +0100

    Add i18n support for paper trade warning
    - Add paperTradeWarning translations to all locale files (en, de, es, fr)
    - Update PaperTradeWarning component to use i18n keys
    - Support for English, German, Spanish, and French translations

--- components/PaperTradeWarning.vue
+++ components/PaperTradeWarning.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const { $t } = useI18n()
 const client = useSupabaseClient()
 
 // Reactive state for showing the warning
@@ -49,19 +50,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="showWarning" class="alert alert-warning shadow-lg w-full flex justify-between">
+  <div v-if="showWarning" class="alert alert-warning shadow-lg w-full">
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z" />
       </svg>
       <div>
-        <h3 class="font-bold">Paper Trade Mode Only</h3>
-        <div class="text-xs">This is Paper Trade Mode Only temporary for Jan to stresstest the system! More infos soon.</div>
+        <h3 class="font-bold">{{ $t('app.paperTradeWarning.title') }}</h3>
+        <div class="text-xs">{{ $t('app.paperTradeWarning.message') }}</div>
       </div>
     </div>
     <div class="flex-none">
-      <button class="btn btn-lg btn-warning bg-yellow-400 hover:bg-yellow-300 px-8" @click="acknowledgeWarning">
-        Ok
+      <button class="btn btn-lg btn-warning" @click="acknowledgeWarning">
+        {{ $t('app.paperTradeWarning.acknowledge') }}
       </button>
     </div>
   </div>
